792123438de7e0a412d84bdb1d1edc13dff60b8a
Surya Duggirala <suryad96@berkeley.edu>
2017-06-14 16:57:54 -0700

    first commit for search app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    const style = {
+      textAlign: "center",
+
+    }
+    return (
+        <h1 style={style}>THE APP IS UP AND RUNNIN</h1>
+    );
+  }
+}
+
+export default App;
